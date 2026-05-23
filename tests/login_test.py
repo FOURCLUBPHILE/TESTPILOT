7cb9faf84591587657a3e74bb8348b78c9f65d8c
FOURCLUBPHILE <fourclubphile@gmail.com>
2026-05-23 13:24:32 +0530

    Configure Chrome driver for headless testing
    Updated the Chrome driver initialization to use headless mode with additional options.

--- tests/login_test.py
+++ tests/login_test.py
@@ -4,8 +4,20 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-# Launch browser
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()),
+    options=options
+)
 
 # Open test website
 driver.get("https://practicetestautomation.com/practice-test-login/")
